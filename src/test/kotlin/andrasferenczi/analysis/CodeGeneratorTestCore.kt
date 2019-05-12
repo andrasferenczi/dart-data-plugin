c86063e6379ec2170a0896b60d62d37dda0fef62
Andrew <a.ferenczi.andras@gmail.com>
2019-05-12 20:29:46 +0200

    Tests are too difficult to create.

---
+++ src/test/kotlin/andrasferenczi/analysis/CodeGeneratorTestCore.kt
@@ -0,0 +1,31 @@
+package andrasferenczi.analysis
+
+import com.intellij.codeInsight.CodeInsightTestCase
+import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.testFramework.LightProjectDescriptor
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
+import com.jetbrains.lang.dart.sdk.DartSdk
+
+class CodeGeneratorTestCore: LightPlatformCodeInsightFixtureTestCase() {
+
+//    fun doSetUp() {
+//        this.setUp()
+//    }
+//
+//    fun doTearDown() {
+//        this.tearDown()
+//    }
+//
+//    override fun getProjectDescriptor(): LightProjectDescriptor {
+//        return object: LightProjectDescriptor() {
+//            override fun getSdk(): Sdk? {
+//                return DartSdk.getDartSdk()
+//            }
+//        }
+//    }
+//
+//    fun doRunTest() {
+//        println("Run test")
+//    }
+
+}
