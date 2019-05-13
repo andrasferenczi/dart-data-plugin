e6449e6c90d0a1af58ea5271e91aecbfc6c7b181
Andrew <a.ferenczi.andras@gmail.com>
2019-05-13 07:08:02 +0200

    Settings menu created

--- src/main/kotlin/andrasferenczi/configuration/configurationUI.kt
+++ src/main/kotlin/andrasferenczi/configuration/configurationUI.kt
@@ -2,6 +2,8 @@ package andrasferenczi.configuration
 
 import com.intellij.uiDesigner.core.GridConstraints
 import com.intellij.uiDesigner.core.GridLayoutManager
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
 import java.awt.Insets
 import javax.swing.*
 
@@ -16,77 +18,87 @@ data class ConfigurationUIInput(
 class ConfigurationUIOutput(
     val jComponent: JComponent,
 
-    val copyWithNameTextField: JTextField
+    val copyWithNameTextField: JTextField,
+    val useRequiredAnnotationCheckBox: JCheckBox,
+    val useNewKeywordCheckbox: JCheckBox
 )
 
 
 fun createConfigurationUI(input: ConfigurationUIInput): ConfigurationUIOutput {
-    val container = JPanel(
-        GridLayoutManager(
-            4,
-            2,
-            Insets(0, 0, 0, 0), 12, 12
-        )
-    )
 
-    container.add(
-        JLabel("copyWith() function name"),
-        GridConstraints().apply {
-            row = 0
-            column = 0
-//            fill = GridConstraints.SIZEPOLICY_CAN_GROW
-//            vSizePolicy = GridConstraints.SIZEPOLICY_CAN_SHRINK
+    val pane = JPanel(GridBagLayout())
+
+    val TOP_INSET = 10
+    val BOTTOM_INSET = 0
+    val RIGHT_INSET = 10
+
+    // The texts should be aligned
+    val CHECKBOX_CHECK_AREA_WIDTH = 22
+    val CHECKBOX_LEFT_INSET = 15
+    val NO_CHECKBOX_LEFT_INSET = CHECKBOX_CHECK_AREA_WIDTH + CHECKBOX_LEFT_INSET
+
+    // ROW 1
+    pane.add(
+        JLabel("name of the copyWith() method"),
+        GridBagConstraints().apply {
+            fill = GridBagConstraints.HORIZONTAL
+            gridx = 0
+            gridy = 0
+            insets = Insets(TOP_INSET, NO_CHECKBOX_LEFT_INSET, BOTTOM_INSET, RIGHT_INSET)
         }
     )
 
-    val copyWithNameTextField = JTextField("copyWith") // Todo: Get this from input
-    container.add(
+    val copyWithNameTextField = JTextField("copyWith")
+    pane.add(
         copyWithNameTextField,
-        GridConstraints().apply {
-            //            hSizePolicy = GridConstraints.SIZEPOLICY_WANT_GROW
-            fill = GridConstraints.FILL_HORIZONTAL
-            anchor = GridConstraints.ANCHOR_WEST
-            row = 0
-            column = 1
-//            vSizePolicy = GridConstraints.SIZEPOLICY_CAN_SHRINK
+        GridBagConstraints().apply {
+            fill = GridBagConstraints.HORIZONTAL
+            weightx = 0.6
+            gridx = 1
+            gridy = 0
+            insets = Insets(TOP_INSET, CHECKBOX_LEFT_INSET, BOTTOM_INSET, RIGHT_INSET)
         }
     )
 
-    val addRequiredToConstructor = JCheckBox("add @required to constructor parameters", true) // Todo: Use input
-    container.add(
-        addRequiredToConstructor,
-        GridConstraints().apply {
-            //            hSizePolicy = GridConstraints.SIZEPOLICY_WANT_GROW
-            fill = GridConstraints.FILL_HORIZONTAL
-            anchor = GridConstraints.ANCHOR_WEST
-            row = 1
-            column = 0
-//            vSizePolicy = GridConstraints.SIZEPOLICY_CAN_SHRINK
+    val useRequiredAnnotationCheckBox = JCheckBox("add @required to constructor parameters", false)
+    // ROW 2
+    pane.add(
+        useRequiredAnnotationCheckBox,
+        GridBagConstraints().apply {
+            fill = GridBagConstraints.HORIZONTAL
+            gridx = 0
+            gridy = 1
+            insets = Insets(TOP_INSET, CHECKBOX_LEFT_INSET, BOTTOM_INSET, RIGHT_INSET)
         }
     )
 
-    val useNewKeywordConstructorCall = JCheckBox("use the 'new' keyword in constructor calls", true) // Todo: Use input
-    container.add(
-        useNewKeywordConstructorCall,
-        GridConstraints().apply {
-            //            hSizePolicy = GridConstraints.SIZEPOLICY_WANT_GROW
-            fill = GridConstraints.FILL_HORIZONTAL
-            anchor = GridConstraints.ANCHOR_WEST
-            row = 2
-            column = 0
-//            vSizePolicy = GridConstraints.SIZEPOLICY_CAN_SHRINK
+    val useNewKeywordCheckbox = JCheckBox("use the 'new' keyword", false)
+    // ROW 3
+    pane.add(
+        useNewKeywordCheckbox,
+        GridBagConstraints().apply {
+            fill = GridBagConstraints.HORIZONTAL
+            gridx = 0
+            gridy = 2
+            insets = Insets(TOP_INSET, CHECKBOX_LEFT_INSET, BOTTOM_INSET, RIGHT_INSET)
         }
     )
 
-
-    val spacer = JPanel()
-    val spacerConstraints = GridConstraints()
-    spacerConstraints.row = 3
-    spacerConstraints.fill = GridConstraints.FILL_BOTH
-    container.add(spacer, spacerConstraints)
+    // Remaining space
+    pane.add(
+        JPanel(),
+        GridBagConstraints().apply {
+            weighty = 1.0   // request any extra vertical space
+            anchor = GridBagConstraints.PAGE_END // bottom of space
+            gridy = 3
+            gridwidth = 2
+        }
+    )
 
     return ConfigurationUIOutput(
-        container,
-        copyWithNameTextField
+        pane,
+        copyWithNameTextField,
+        useRequiredAnnotationCheckBox,
+        useNewKeywordCheckbox
     )
 }
--- src/test/kotlin/andrasferenczi/configuration_ui/configuration_user_test.kt
+++ src/test/kotlin/andrasferenczi/configuration_ui/configuration_user_test.kt
@@ -1,13 +1,13 @@
 package andrasferenczi.configuration_ui
 
+import andrasferenczi.configuration.ConfigurationUIInput
+import andrasferenczi.configuration.createConfigurationUI
 import com.intellij.uiDesigner.core.GridConstraints
 import com.intellij.uiDesigner.core.GridLayoutManager
-import javax.swing.JFrame
 import java.awt.Insets
-import javax.swing.JPanel
-import javax.swing.JButton
 import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
+import javax.swing.*
 
 
 fun showGridBagLayout(shouldFill: Boolean, shouldWeightX: Boolean) {
@@ -22,55 +22,116 @@ fun showGridBagLayout(shouldFill: Boolean, shouldWeightX: Boolean) {
 
     var button: JButton
 
-    pane.setLayout(GridBagLayout())
+    pane.layout = GridBagLayout().apply {
+
+    }
+
     val c = GridBagConstraints()
     if (shouldFill) {
         //natural height, maximum width
         c.fill = GridBagConstraints.HORIZONTAL
     }
 
-    button = JButton("Button 1")
     if (shouldWeightX) {
         c.weightx = 0.5
     }
-    c.fill = GridBagConstraints.HORIZONTAL
-    c.gridx = 0
-    c.gridy = 0
-    pane.add(button, c)
 
-    button = JButton("Button 2")
-    c.fill = GridBagConstraints.HORIZONTAL
-    c.weightx = 0.5
-    c.gridx = 1
-    c.gridy = 0
-    pane.add(button, c)
+    val TOP_INSET = 10
+    val BOTTOM_INSET = 0
+    val RIGHT_INSET = 10
 
-    button = JButton("Button 3")
-    c.fill = GridBagConstraints.HORIZONTAL
-    c.weightx = 0.5
-    c.gridx = 2
-    c.gridy = 0
-    pane.add(button, c)
+    // The texts should be aligned
+    val CHECKBOX_CHECK_AREA_WIDTH = 22
+    val CHECKBOX_LEFT_INSET = 15
+    val NO_CHECKBOX_LEFT_INSET = CHECKBOX_CHECK_AREA_WIDTH + CHECKBOX_LEFT_INSET
 
-    button = JButton("Long-Named Button 4")
-    c.fill = GridBagConstraints.HORIZONTAL
-    c.ipady = 40      //make this component tall
-    c.weightx = 0.0
-    c.gridwidth = 3
-    c.gridx = 0
-    c.gridy = 1
-    pane.add(button, c)
+    // ROW 1
+    pane.add(
+        JLabel("copyWith() function name"),
+        GridBagConstraints().apply {
+            fill = GridBagConstraints.HORIZONTAL
+            gridx = 0
+            gridy = 0
+            insets = Insets(TOP_INSET, NO_CHECKBOX_LEFT_INSET, BOTTOM_INSET, RIGHT_INSET)
+        }
+    )
 
-    button = JButton("5")
-    c.fill = GridBagConstraints.HORIZONTAL
-    c.ipady = 0       //reset to default
-    c.weighty = 1.0   //request any extra vertical space
-    c.anchor = GridBagConstraints.PAGE_END //bottom of space
-    c.insets = Insets(10, 0, 0, 0)  //top padding
-    c.gridx = 1       //aligned with button 2
-    c.gridwidth = 2   //2 columns wide
-    c.gridy = 2       //third row
-    pane.add(button, c)
+    pane.add(
+        JTextField("copyWith"),
+        GridBagConstraints().apply {
+            fill = GridBagConstraints.HORIZONTAL
+            weightx = 0.6
+            gridx = 1
+            gridy = 0
+            insets = Insets(TOP_INSET, CHECKBOX_LEFT_INSET, BOTTOM_INSET, RIGHT_INSET)
+        }
+    )
+
+    // ROW 2
+    pane.add(
+        JCheckBox("add @required to constructor parameters", false),
+        GridBagConstraints().apply {
+            fill = GridBagConstraints.HORIZONTAL
+            gridx = 0
+            gridy = 1
+            insets = Insets(TOP_INSET, CHECKBOX_LEFT_INSET, BOTTOM_INSET, RIGHT_INSET)
+        }
+    )
+
+    // ROW 3
+    pane.add(
+        JCheckBox("use the 'new' keyword", false),
+        GridBagConstraints().apply {
+            fill = GridBagConstraints.HORIZONTAL
+            gridx = 0
+            gridy = 2
+            insets = Insets(TOP_INSET, CHECKBOX_LEFT_INSET, BOTTOM_INSET, RIGHT_INSET)
+        }
+    )
+
+//    button = JButton("Button 2")
+//    c.fill = GridBagConstraints.HORIZONTAL
+//    c.weightx = 0.5
+//    c.gridx = 1
+//    c.gridy = 0
+//    pane.add(button, c)
+//
+//    button = JButton("Button 3")
+//    c.fill = GridBagConstraints.HORIZONTAL
+//    c.weightx = 0.5
+//    c.gridx = 2
+//    c.gridy = 0
+//    pane.add(button, c)
+//
+//    button = JButton("Long-Named Button 4")
+//    c.fill = GridBagConstraints.HORIZONTAL
+//    c.ipady = 40      //make this component tall
+//    c.weightx = 0.0
+//    c.gridwidth = 3
+//    c.gridx = 0
+//    c.gridy = 1
+//    pane.add(button, c)
+//
+//    button = JButton("5")
+//    c.fill = GridBagConstraints.HORIZONTAL
+//    c.ipady = 0       //reset to default
+//    c.weighty = 1.0   //request any extra vertical space
+//    c.anchor = GridBagConstraints.PAGE_END //bottom of space
+//    c.insets = Insets(10, 0, 0, 0)  //top padding
+//    c.gridx = 1       //aligned with button 2
+//    c.gridwidth = 2   //2 columns wide
+//    c.gridy = 2       //third row
+//    pane.add(button, c)
+
+    pane.add(
+        JPanel(),
+        GridBagConstraints().apply {
+            weighty = 1.0   //request any extra vertical space
+            anchor = GridBagConstraints.PAGE_END //bottom of space
+            gridy = 3
+            gridwidth = 2
+        }
+    )
 
 
     JFrame.setDefaultLookAndFeelDecorated(true)
@@ -87,21 +148,21 @@ fun showGridBagLayout(shouldFill: Boolean, shouldWeightX: Boolean) {
  * For faster preview compared to starting the entire IDEA anew
  */
 fun main() {
-//    val component = createConfigurationUI(ConfigurationUIInput.TEST_DATA).jComponent
-//
-//    // create a basic JFrame
-//    JFrame.setDefaultLookAndFeelDecorated(true)
-//    val frame = JFrame("Fast JComponent preview")
-//    frame.setSize(700, 500)
-//    frame.setLocation(450, 350)
-//    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-//
-//    // add the JComponent to main frame
-//    frame.add(component)
-//
-//    frame.isVisible = true
+    val component = createConfigurationUI(ConfigurationUIInput.TEST_DATA).jComponent
 
-    showGridBagLayout(true, true)
+    // create a basic JFrame
+    JFrame.setDefaultLookAndFeelDecorated(true)
+    val frame = JFrame("Fast JComponent preview")
+    frame.setSize(700, 500)
+    frame.setLocation(450, 350)
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+
+    // add the JComponent to main frame
+    frame.add(component)
+
+    frame.isVisible = true
+
+//    showGridBagLayout(false, false)
 
 //    val frame = JFrame("GridLayout Demo")
 //    // create grid layout with 3 rows , 2 columns with horizontal
